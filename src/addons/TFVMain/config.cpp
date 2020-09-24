7e614e7c6fd46fa48ee714042f523aff127ff68b
Peter <petergorman11@gmail.com>
2020-09-24 15:53:51 -0400

    Add TFVMain addon

---
+++ src/addons/TFVMain/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches
+{
+    class TFVMain
+    {
+        name = "Task Force Vandal Aux - Main";
+        author = "[TW] gunman435";
+
+        requiredVersion = 0.1;
+        requiredAddons[] = {};
+
+        units[]={};
+        weapons[]={};
+    };
+};
+
+class cfgMods
+{
+    author = "[TW] gunman435";
+};
