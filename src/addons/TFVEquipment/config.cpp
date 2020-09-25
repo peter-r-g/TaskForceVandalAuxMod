75407d5f2b81533cc7f26151be5854b7b985944d
Peter <petergorman11@gmail.com>
2020-09-25 15:39:06 -0400

    Add ONI rucksack

--- src/addons/TFVEquipment/config.cpp
+++ src/addons/TFVEquipment/config.cpp
@@ -12,7 +12,8 @@ class CfgPatches
         };
 
         units[] = {
-            "TFV_OPTRE_ILCS_Rucksack_LR"
+            "TFV_OPTRE_ILCS_Rucksack_LR",
+            "TFV_OPTRE_ONI_Rucksack"
         };
         weapons[] = {};
     };
@@ -21,12 +22,28 @@ class CfgPatches
 class CfgVehicles
 {
     class OPTRE_ILCS_Rucksack_Black;
+    class OPTRE_UNSC_Rucksack;
     class TFV_OPTRE_ILCS_Rucksack_LR: OPTRE_ILCS_Rucksack_Black
     {
         author = "Article 2 Studios & [TW] gunman435";
         displayName = "ILCS Rucksack [LR]";
         descriptionShort = "ILCS Rucksack [LR]<br>ODST Long Range Radio<br>25km Effective Range";
         maximumLoad=300;
+		mass=50;
+        tf_encryptionCode = "tf_west_radio_code";
+		tf_dialog = "rt1523g_radio_dialog";
+		tf_subtype = "digital_lr";
+		tf_range = 25000;
+		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+		tf_hasLRradio = 1;
+    };
+
+    class TFV_OPTRE_ONI_Rucksack: OPTRE_UNSC_Rucksack
+    {
+        author = "Article 2 Studios & [TW] gunman435";
+        displayName = "ONI Rucksack";
+        descriptionShort = "ONI Rucksack<br>ONI Long Range Radio<br>25km Effective Range";
+        maximumLoad=400;
 		mass=50;
         tf_encryptionCode = "tf_west_radio_code";
 		tf_dialog = "rt1523g_radio_dialog";
