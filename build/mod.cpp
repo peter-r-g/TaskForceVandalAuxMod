cdb28554de9679865ce523d5b84f1bf3dd882078
Peter <petergorman11@gmail.com>
2020-09-25 13:12:43 -0400

    Remove build section

--- build/mod.cpp
+++
@@ -1,8 +0,0 @@
-name = "Task Force Vandal Auxiliary Mod";
-tooltipOwned="Task Force Vandal Aux";
-picture = "TFVMain\logo.paa";
-logoSmall = "TFVMain\logo.paa";
-logo = "TFVMain\logo.paa";
-logoOver = "TFVMain\logo.paa";
-hideName = 0;
-hidePicture = 0;
