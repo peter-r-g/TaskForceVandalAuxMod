b967a6bc00b99c843d67e3dade854c7fb7258071
Peter <petergorman11@gmail.com>
2020-09-26 12:19:08 -0400

    Port Halo2A music mod

--- build_windows.bat
+++ build_windows.bat
@@ -1,13 +1,13 @@
 @ECHO OFF
-
 set ADDONBUILDER_EXE=E:\Steam Games\steamapps\common\Arma 3 Tools\AddonBuilder\AddonBuilder.exe
 set WORKING_DIR=E:\GitHub\TaskForceVandalAuxMod
 
 mkdir build\addons
-mkdir build\key
+mkdir build\keys
 "%ADDONBUILDER_EXE%" "%WORKING_DIR%/src/addons/TFVMain" "%WORKING_DIR%/build/addons/" -sign="%WORKING_DIR%/TaskForceVandalAuxMod.biprivatekey"
 "%ADDONBUILDER_EXE%" "%WORKING_DIR%/src/addons/TFVEquipment" "%WORKING_DIR%/build/addons/" -sign="%WORKING_DIR%/TaskForceVandalAuxMod.biprivatekey"
 "%ADDONBUILDER_EXE%" "%WORKING_DIR%/src/addons/TFVResupply" "%WORKING_DIR%/build/addons/" -sign="%WORKING_DIR%/TaskForceVandalAuxMod.biprivatekey"
+"%ADDONBUILDER_EXE%" "%WORKING_DIR%/src/addons/TFVMusic" "%WORKING_DIR%/build/addons/" -sign="%WORKING_DIR%/TaskForceVandalAuxMod.biprivatekey"
 copy /Y "%WORKING_DIR%\src\mod.cpp" "%WORKING_DIR%\build\mod.cpp"
-copy /Y "%WORKING_DIR%\src\key\TaskForceVandalAuxMod.bikey" "%WORKING_DIR%\build\key\TaskForceVandalAuxMod.bikey"
+copy /Y "%WORKING_DIR%\src\keys\TaskForceVandalAuxMod.bikey" "%WORKING_DIR%\build\keys\TaskForceVandalAuxMod.bikey"
 pause
---
+++ src/addons/TFVMusic/config.cpp
@@ -0,0 +1,260 @@
+class CfgPatches
+{
+	class TFVMusic
+	{
+		name = "Task Force Vandal Aux - Music";
+        author = "Callum & [TW] gunman435";
+
+        requiredVersion = 0.1;
+        requiredAddons[] = {
+            "TFVMain"
+		};
+
+        units[] = {};
+        weapons[] = {};
+	};
+};
+
+class CfgMusicClasses
+{
+	class TFVMusic
+	{
+		displayName = "Task Force Vandal - Halo Music";
+	};
+};
+
+class CfgMusic
+{
+	class TFVHaloThemeGungnirMix
+	{
+		name = "Halo Theme Gungnir Mix";
+		sound[] = {"\TFVMusic\music\HaloThemeGungnirMix.ogg",1,1};
+		theme = "Score";
+		duration = "255";
+		musicClass = "TFVMusic";
+	};
+	class TFVSkyline
+	{
+		name = "Skyline";
+		sound[] = {"\TFVMusic\music\Skyline.ogg",1,1};
+		theme = "Score";
+		duration = "95";
+		musicClass = "TFVMusic";
+	};
+	class TFVNotaNumber
+	{
+		name = "Not a Number";
+		sound[] = {"\TFVMusic\music\NotaNumber.ogg",1,1};
+		theme = "Score";
+		duration = "95";
+		musicClass = "TFVMusic";
+	};
+	class TFVKilindiniHarbour
+	{
+		name = "Kilindini Harbour";
+		sound[] = {"\TFVMusic\music\KilindiniHarbour.ogg",1,1};
+		theme = "Score";
+		duration = "188";
+		musicClass = "TFVMusic";
+	};
+	class TFVOnlyaStarOnlytheSea
+	{
+		name = "Only a Star, Only the Sea";
+		sound[] = {"\TFVMusic\music\OnlyaStarOnlytheSea.ogg",1,1};
+		theme = "Score";
+		duration = "116";
+		musicClass = "TFVMusic";
+	};
+	class TFVASpartanRises
+	{
+		name = "A Spartan Rises";
+		sound[] = {"\TFVMusic\music\ASpartanRises.ogg",1,1};
+		theme = "Score";
+		duration = "130";
+		musicClass = "TFVMusic";
+	};
+	class TFVUnforgottenMemories
+	{
+		name = "Unforgotten Memories";
+		sound[] = {"\TFVMusic\music\UnforgottenMemories.ogg",1,1};
+		theme = "Score";
+		duration = "144";
+		musicClass = "TFVMusic";
+	};
+	class TFVSecondPrelude
+	{
+		name = "Second Prelude";
+		sound[] = {"\TFVMusic\music\SecondPrelude.ogg",1,1};
+		theme = "Score";
+		duration = "110";
+		musicClass = "TFVMusic";
+	};
+	class TFVThisGlitteringBand
+	{
+		name = "This Glittering Band";
+		sound[] = {"\TFVMusic\music\ThisGlitteringBand.ogg",1,1};
+		theme = "Score";
+		duration = "153";
+		musicClass = "TFVMusic";
+	};
+	class TFVJeopardy
+	{
+		name = "Jeopardy";
+		sound[] = {"\TFVMusic\music\Jeopardy.ogg",1,1};
+		theme = "Score";
+		duration = "168";
+		musicClass = "TFVMusic";
+	};
+	class TFVHaloThemeScorpionMix
+	{
+		name = "Halo Theme Scorpion Mix";
+		sound[] = {"\TFVMusic\music\HaloThemeScorpionMix.ogg",1,1};
+		theme = "Score";
+		duration = "187";
+		musicClass = "TFVMusic";
+	};
+	class TFVPunishment
+	{
+		name = "Punishment";
+		sound[] = {"\TFVMusic\music\Punishment.ogg",1,1};
+		theme = "Score";
+		duration = "157";
+		musicClass = "TFVMusic";
+	};
+	class TFVPromisetheGirl
+	{
+		name = "Promise the Girl";
+		sound[] = {"\TFVMusic\music\PromisetheGirl.ogg",1,1};
+		theme = "Score";
+		duration = "67";
+		musicClass = "TFVMusic";
+	};
+	class TFVUnsulliedMemory
+	{
+		name = "Unsullied Memory";
+		sound[] = {"\TFVMusic\music\UnsulliedMemory.ogg",1,1};
+		theme = "Score";
+		duration = "78";
+		musicClass = "TFVMusic";
+	};
+	class TFVAriseInValor
+	{
+		name = "Arise In Valor";
+		sound[] = {"\TFVMusic\music\AriseInValor.ogg",1,1};
+		theme = "Score";
+		duration = "189";
+		musicClass = "TFVMusic";
+	};
+	class TFVUnweariedHeart
+	{
+		name = "Unwearied Heart";
+		sound[] = {"\TFVMusic\music\UnweariedHeart.ogg",1,1};
+		theme = "Score";
+		duration = "100";
+		musicClass = "TFVMusic";
+	};
+	class TFVSpartansRegret
+	{
+		name = "Spartans Regret";
+		sound[] = {"\TFVMusic\music\SpartansRegret.ogg",1,1};
+		theme = "Score";
+		duration = "36";
+		musicClass = "TFVMusic";
+	};
+	class TFVGenesong
+	{
+		name = "Genesong";
+		sound[] = {"\TFVMusic\music\Genesong.ogg",1,1};
+		theme = "Score";
+		duration = "184";
+		musicClass = "TFVMusic";
+	};
+	class TFVImpart
+	{
+		name = "Impart";
+		sound[] = {"\TFVMusic\music\Impart.ogg",1,1};
+		theme = "Score";
+		duration = "146";
+		musicClass = "TFVMusic";
+	};
+	class TFVMoonOverMombasa
+	{
+		name = "Moon Over Mombasa";
+		sound[] = {"\TFVMusic\music\MoonOverMombasa.ogg",1,1};
+		theme = "Score";
+		duration = "117";
+		musicClass = "TFVMusic";
+	};
+	class TFVTrappedInAmber
+	{
+		name = "Trapped In Amber";
+		sound[] = {"\TFVMusic\music\TrappedInAmber.ogg",1,1};
+		theme = "Score";
+		duration = "146";
+		musicClass = "TFVMusic";
+	};
+	class TFVBuildersLegacy
+	{
+		name = "Builders Legacy";
+		sound[] = {"\TFVMusic\music\BuildersLegacy.ogg",1,1};
+		theme = "Score";
+		duration = "118";
+		musicClass = "TFVMusic";
+	};
+	class TFVMoonOverMombasaPart2
+	{
+		name = "Moon Over Mombasa Part 2";
+		sound[] = {"\TFVMusic\music\MoonOverMombasaPart2.ogg",1,1};
+		theme = "Score";
+		duration = "71";
+		musicClass = "TFVMusic";
+	};
+	class TFVZealousChampion
+	{
+		name = "Zealous Champion";
+		sound[] = {"\TFVMusic\music\ZealousChampion.ogg",1,1};
+		theme = "Score";
+		duration = "153";
+		musicClass = "TFVMusic";
+	};
+	class TFVStewardShepherdLonelySoul
+	{
+		name = "Steward, Shepherd, LonelySoul";
+		sound[] = {"\TFVMusic\music\StewardShepherdLonelySoul.ogg",1,1};
+		theme = "Score";
+		duration = "75";
+		musicClass = "TFVMusic";
+	};
+	class TFVAfricaSuite
+	{
+		name = "Africa Suite";
+		sound[] = {"\TFVMusic\music\AfricaSuite.ogg",1,1};
+		theme = "Score";
+		duration = "70";
+		musicClass = "TFVMusic";
+	};
+	class TFVCrackedLegend
+	{
+		name = "Cracked Legend";
+		sound[] = {"\TFVMusic\music\CrackedLegend.ogg",1,1};
+		theme = "Score";
+		duration = "97";
+		musicClass = "TFVMusic";
+	};
+	class TFVDrumsOfReachRedux
+	{
+		name = "Drums Of Reach Redux";
+		sound[] = {"\TFVMusic\music\DrumsOfReachRedux.ogg",1,1};
+		theme = "Score";
+		duration = "72";
+		musicClass = "TFVMusic";
+	};
+	class TFVGhostsOfReachRedux
+	{
+		name = "Ghosts Of Reach Redux";
+		sound[] = {"\TFVMusic\music\GhostsOfReachRedux.ogg",1,1};
+		theme = "Score";
+		duration = "148";
+		musicClass = "TFVMusic";
+	};
+};
