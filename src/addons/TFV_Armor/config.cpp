c6fe359355c444c2c089468ef3d165d1f00e7fc3
Peter <petergorman11@gmail.com>
2022-08-07 19:13:58 -0400

    Update vest macro

--- src/addons/TFV_Armor/config.cpp
+++ src/addons/TFV_Armor/config.cpp
@@ -180,11 +180,11 @@ class CfgWeapons
         "TFV_Armor\data\aasen\aasen_legs_co.paa", \
         "optre_unsc_units\army\data\ghillie_woodland_co.paa", \
         "TFV_Armor\data\aasen\aasen_odst_armour_co.paa", \
-    };
+    }
     class TFV_M52D_Vest_Light_Aasen: OPTRE_UNSC_M52D_Armor_Light
     {
         displayName = "M52D Body Armor (Aasen)";
-        VEST_AASEN
+        VEST_AASEN;
     };
     #pragma endregion Aasen
 
@@ -221,17 +221,17 @@ class CfgWeapons
         "TFV_Armor\data\castle\castle_legs_co.paa", \
         "optre_unsc_units\army\data\ghillie_woodland_co.paa", \
         "TFV_Armor\data\castle\castle_odst_armour_co.paa", \
-    };
+    }
     class TFV_M52D_Vest_Light_Castle: OPTRE_UNSC_M52D_Armor_Light
     {
         displayName = "M52D Body Armor (Light) (Castle)";
-        VEST_CASTLE
+        VEST_CASTLE;
     };
 
     class TFV_M52D_Vest_Rifleman_Castle: OPTRE_UNSC_M52D_Armor_Rifleman
     {
         displayName = "M52D Body Armor (Rifleman) (Castle)";
-        VEST_CASTLE
+        VEST_CASTLE;
     };
     #pragma endregion Castle
 
@@ -268,29 +268,29 @@ class CfgWeapons
         "TFV_Armor\data\reeves\reeves_legs_co.paa", \
         "optre_unsc_units\army\data\ghillie_woodland_co.paa", \
         "TFV_Armor\data\reeves\reeves_odst_armour_co.paa", \
-    };
+    }
     class TFV_M52D_Vest_Demolitions_Reeves: TFV_OPTRE_UNSC_M52D_Armor_Demolitions
     {
         displayName = "M52D Body Armor (Demolitions) (Reeves)";
-        VEST_REEVES
+        VEST_REEVES;
     };
 
     class TFV_M52D_Vest_Light_Reeves: OPTRE_UNSC_M52D_Armor_Light
     {
         displayName = "M52D Body Armor (Light) (Reeves)";
-        VEST_REEVES
     }
+        VEST_REEVES;
 
     class TFV_M52D_Vest_Rifleman_Reeves: OPTRE_UNSC_M52D_Armor_Rifleman
     {
         displayName = "M52D Body Armor (Rifleman) (Reeves)";
-        VEST_REEVES
+        VEST_REEVES;
     };
 
     class TFV_M52D_Vest_Scout_Reeves: OPTRE_UNSC_M52D_Armor_Scout
     {
         displayName = "M52D Body Armor (Scout) (Reeves)";
-        VEST_REEVES
+        VEST_REEVES;
     };
     #pragma endregion Reeves
 
@@ -327,53 +327,53 @@ class CfgWeapons
         "TFV_Armor\data\roy\roy_legs_co.paa", \
         "optre_unsc_units\army\data\ghillie_woodland_co.paa", \
         "TFV_Armor\data\roy\roy_odst_armour_co.paa", \
-    };
+    }
     class TFV_M52D_Vest_Basic_Roy: OPTRE_UNSC_M52D_Armor
     {
         displayName = "M52D Body Armor (Basic) (Roy)";
-        VEST_ROY
+        VEST_ROY;
     };
 
     class TFV_M52D_Vest_Demolitions_Roy: TFV_OPTRE_UNSC_M52D_Armor_Demolitions
     {
         displayName = "M52D Body Armor (Demolitions) (Roy)";
-        VEST_ROY
+        VEST_ROY;
     };
 
     class TFV_M52D_Vest_Light_Roy: OPTRE_UNSC_M52D_Armor_Light
     {
         displayName = "M52D Body Armor (Light) (Roy)";
-        VEST_ROY
+        VEST_ROY;
     };
 
     class TFV_M52D_Vest_Marksman_Roy: OPTRE_UNSC_M52D_Armor_Marksman
     {
         displayName = "M52D Body Armor (Marksman) (Roy)";
-        VEST_ROY
+        VEST_ROY;
     };
 
     class TFV_M52D_Vest_Medic_Roy: OPTRE_UNSC_M52D_Armor_Medic
     {
         displayName = "M52D Body Armor (Medic) (Roy)";
-        VEST_ROY
+        VEST_ROY;
     };
 
     class TFV_M52D_Vest_Rifleman_Roy: OPTRE_UNSC_M52D_Armor_Rifleman
     {
         displayName = "M52D Body Armor (Rifleman) (Roy)";
-        VEST_ROY
+        VEST_ROY;
     };
 
     class TFV_M52D_Vest_Scout_Roy: OPTRE_UNSC_M52D_Armor_Scout
     {
         displayName = "M52D Body Armor (Scout) (Roy)";
-        VEST_ROY
+        VEST_ROY;
     };
 
     class TFV_M52D_Vest_Sniper_Roy: OPTRE_UNSC_M52D_Armor_Sniper
     {
         displayName = "M52D Body Armor (Sniper) (Roy)";
-        VEST_ROY
+        VEST_ROY;
     };
     #pragma endregion Roy
 };
