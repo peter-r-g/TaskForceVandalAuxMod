1fed315e854ec6a30ab0f61ed2190b3bf583fa58
Peter <petergorman11@gmail.com>
2022-08-07 19:15:25 -0400

    Add tacpad to all of Reeves' vests

--- src/addons/TFV_Armor/config.cpp
+++ src/addons/TFV_Armor/config.cpp
@@ -284,9 +284,89 @@ class CfgWeapons
     class TFV_M52D_Vest_Demolitions_Reeves: TFV_OPTRE_UNSC_M52D_Armor_Demolitions
     {
         displayName = "M52D Body Armor (Demolitions) (Reeves)";
+        hiddenSelections[]=
+		{
+			"camo",
+			"camo2",
+			"camo3",
+			"camo4",
+			"camo5",
+			"A_KneesMarLeft",
+			"A_KneesMarRight",
+			"A_Ghillie",
+			"AS_LargeLeft",
+			"AS_LargeRight",
+			"AS_MediumLeft",
+			"AS_MediumRight",
+			"AS_ODSTLeft",
+			"AS_ODSTCQBRight",
+			"AS_ODSTSniperLeft",
+			"AS_ODSTSniperRight",
+			"AS_SmallLeft",
+			"AS_SmallRight",
+			"AP_AR",
+			"AP_BR",
+			"AP_Canteen",
+			"AP_Knife",
+			"AP_MGThigh",
+			"AP_AR",
+			"AP_Pack",
+			"AP_Pistol",
+			"AP_Rounds",
+			"AP_SG",
+			"AP_SMG",
+			"AP_Sniper",
+			"AP_Thigh",
+			"AP_Frag",
+			"AP_Smoke",
+			"APO_AR",
+			"APO_BR",
+			"APO_Sniper",
+			"CustomKit_Scorch"
+		};
         VEST_REEVES;
         class ItemInfo: ItemInfo
         {
+            hiddenSelections[]=
+            {
+                "camo",
+                "camo2",
+                "camo3",
+                "camo4",
+                "camo5",
+                "A_KneesMarLeft",
+                "A_KneesMarRight",
+                "A_Ghillie",
+                "AS_LargeLeft",
+                "AS_LargeRight",
+                "AS_MediumLeft",
+                "AS_MediumRight",
+                "AS_ODSTLeft",
+                "AS_ODSTCQBRight",
+                "AS_ODSTSniperLeft",
+                "AS_ODSTSniperRight",
+                "AS_SmallLeft",
+                "AS_SmallRight",
+                "AP_AR",
+                "AP_BR",
+                "AP_Canteen",
+                "AP_Knife",
+                "AP_MGThigh",
+                "AP_AR",
+                "AP_Pack",
+                "AP_Pistol",
+                "AP_Rounds",
+                "AP_SG",
+                "AP_SMG",
+                "AP_Sniper",
+                "AP_Thigh",
+                "AP_Frag",
+                "AP_Smoke",
+                "APO_AR",
+                "APO_BR",
+                "APO_Sniper",
+                "CustomKit_Scorch"
+            };
             VEST_REEVES;
         };
     };
@@ -304,9 +384,89 @@ class CfgWeapons
     class TFV_M52D_Vest_Rifleman_Reeves: OPTRE_UNSC_M52D_Armor_Rifleman
     {
         displayName = "M52D Body Armor (Rifleman) (Reeves)";
+        hiddenSelections[]=
+		{
+			"camo",
+			"camo2",
+			"camo3",
+			"camo4",
+			"camo5",
+			"A_KneesMarLeft",
+			"A_KneesMarRight",
+			"A_Ghillie",
+			"AS_LargeLeft",
+			"AS_LargeRight",
+			"AS_MediumLeft",
+			"AS_MediumRight",
+			"AS_ODSTCQBLeft",
+			"AS_ODSTCQBRight",
+			"AS_ODSTSniperLeft",
+			"AS_ODSTSniperRight",
+			"AS_SmallLeft",
+			"AS_SmallRight",
+			"AP_AR",
+			"AP_BR",
+			"AP_Canteen",
+			"AP_GL",
+			"AP_Knife",
+			"AP_MGThigh",
+			"AP_AR",
+			"AP_Pack",
+			"AP_Pistol",
+			"AP_Rounds",
+			"AP_SG",
+			"AP_SMG",
+			"AP_Sniper",
+			"AP_Thigh",
+			"APO_BR",
+			"APO_Knife",
+			"APO_SMG",
+			"APO_Sniper",
+			"CustomKit_Scorch"
+		};
         VEST_REEVES;
         class ItemInfo: ItemInfo
         {
+            hiddenSelections[]=
+            {
+                "camo",
+                "camo2",
+                "camo3",
+                "camo4",
+                "camo5",
+                "A_KneesMarLeft",
+                "A_KneesMarRight",
+                "A_Ghillie",
+                "AS_LargeLeft",
+                "AS_LargeRight",
+                "AS_MediumLeft",
+                "AS_MediumRight",
+                "AS_ODSTCQBLeft",
+                "AS_ODSTCQBRight",
+                "AS_ODSTSniperLeft",
+                "AS_ODSTSniperRight",
+                "AS_SmallLeft",
+                "AS_SmallRight",
+                "AP_AR",
+                "AP_BR",
+                "AP_Canteen",
+                "AP_GL",
+                "AP_Knife",
+                "AP_MGThigh",
+                "AP_AR",
+                "AP_Pack",
+                "AP_Pistol",
+                "AP_Rounds",
+                "AP_SG",
+                "AP_SMG",
+                "AP_Sniper",
+                "AP_Thigh",
+                "APO_BR",
+                "APO_Knife",
+                "APO_SMG",
+                "APO_Sniper",
+                "CustomKit_Scorch"
+            };
             VEST_REEVES;
         };
     };
@@ -314,9 +474,89 @@ class CfgWeapons
     class TFV_M52D_Vest_Scout_Reeves: OPTRE_UNSC_M52D_Armor_Scout
     {
         displayName = "M52D Body Armor (Scout) (Reeves)";
+        hiddenSelections[]=
+		{
+			"camo",
+			"camo2",
+			"camo3",
+			"camo4",
+			"camo5",
+			"A_KneesMarLeft",
+			"A_KneesMarRight",
+			"A_Ghillie",
+			"AS_LargeLeft",
+			"AS_LargeRight",
+			"AS_MediumLeft",
+			"AS_MediumRight",
+			"AS_ODSTCQBLeft",
+			"AS_ODSTCQBRight",
+			"AS_ODSTSniperLeft",
+			"AS_ODSTSniperRight",
+			"AS_SmallLeft",
+			"AS_SmallRight",
+			"AP_AR",
+			"AP_BR",
+			"AP_Canteen",
+			"AP_GL",
+			"AP_Knife",
+			"AP_MGThigh",
+			"AP_AR",
+			"AP_Pack",
+			"AP_Pistol",
+			"AP_Rounds",
+			"AP_SG",
+			"AP_SMG",
+			"AP_Sniper",
+			"AP_Thigh",
+			"AP_Frag",
+			"APO_AR",
+			"APO_BR",
+			"APO_Sniper",
+			"CustomKit_Scorch"
+		};
         VEST_REEVES;
         class ItemInfo: ItemInfo
         {
+            hiddenSelections[]=
+            {
+                "camo",
+                "camo2",
+                "camo3",
+                "camo4",
+                "camo5",
+                "A_KneesMarLeft",
+                "A_KneesMarRight",
+                "A_Ghillie",
+                "AS_LargeLeft",
+                "AS_LargeRight",
+                "AS_MediumLeft",
+                "AS_MediumRight",
+                "AS_ODSTCQBLeft",
+                "AS_ODSTCQBRight",
+                "AS_ODSTSniperLeft",
+                "AS_ODSTSniperRight",
+                "AS_SmallLeft",
+                "AS_SmallRight",
+                "AP_AR",
+                "AP_BR",
+                "AP_Canteen",
+                "AP_GL",
+                "AP_Knife",
+                "AP_MGThigh",
+                "AP_AR",
+                "AP_Pack",
+                "AP_Pistol",
+                "AP_Rounds",
+                "AP_SG",
+                "AP_SMG",
+                "AP_Sniper",
+                "AP_Thigh",
+                "AP_Frag",
+                "APO_AR",
+                "APO_BR",
+                "APO_Sniper",
+                "CustomKit_Scorch"
+            };
             VEST_REEVES;
         };
     };
