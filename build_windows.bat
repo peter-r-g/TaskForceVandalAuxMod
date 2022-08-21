92f2b69faa197d59b726b02aef0617cc4a021722
Peter <petergorman11@gmail.com>
2022-08-20 23:24:00 -0400

    Big cleanup of build script

--- build_windows.bat
+++ build_windows.bat
@@ -1,33 +1,30 @@
 @ECHO OFF
 
 rem ### Configuration Options.
-rem ## The directory that this batch file is running in.
-set WORKING_DIR=D:\GitHub\Arma\TaskForceVandalAuxMod
 rem ## The path to the addon builder executable.
-set ADDONBUILDER_EXE=E:\Steam Games\steamapps\common\Arma 3 Tools\AddonBuilder\AddonBuilder.exe
+set ADDONBUILDER_EXE="E:\Steam Games\steamapps\common\Arma 3 Tools\AddonBuilder\AddonBuilder.exe"
 
-rem ## Uncomment next two lines if you want to see the help menu.
-rem "%ADDONBUILDER_EXE%" -help
-rem pause
-rem ##
+
+
+
 
 rem ### Don't edit below here.
-rem ## Delete old build folder.
+rem ## Delete old build directories.
 rmdir /S/Q build
+rmdir /S/Q temp
+
 rem ## Create build directories.
+mkdir temp
 mkdir build\addons
 mkdir build\keys
+
 rem ## Build PBOs.
-rem # TFV_Main
-"%ADDONBUILDER_EXE%" "%WORKING_DIR%/src/addons/TFV_Main" "%WORKING_DIR%/build/addons/" -sign="%WORKING_DIR%/TaskForceVandalAuxMod.biprivatekey" -include="%WORKING_DIR%/src/addons/TFV_Main/include.txt"
-rem # TFV_Armor
-"%ADDONBUILDER_EXE%" "%WORKING_DIR%/src/addons/TFV_Armor" "%WORKING_DIR%/build/addons/" -sign="%WORKING_DIR%/TaskForceVandalAuxMod.biprivatekey" -include="%WORKING_DIR%/src/addons/TFV_Armor/include.txt"
-rem # TFV_Equipment
-"%ADDONBUILDER_EXE%" "%WORKING_DIR%/src/addons/TFV_Equipment" "%WORKING_DIR%/build/addons/" -sign="%WORKING_DIR%/TaskForceVandalAuxMod.biprivatekey" -include="%WORKING_DIR%/src/addons/TFV_Equipment/include.txt"
-rem # TFV_Music
-"%ADDONBUILDER_EXE%" "%WORKING_DIR%/src/addons/TFV_Music" "%WORKING_DIR%/build/addons/" -sign="%WORKING_DIR%/TaskForceVandalAuxMod.biprivatekey" -include="%WORKING_DIR%/src/addons/TFV_Music/include.txt"
+for /D %%a in (%cd%\src\addons\*) do (
+    %ADDONBUILDER_EXE% "%%a" "%cd%/build/addons/" -sign="%cd%/TaskForceVandalAuxMod.biprivatekey" -include="%%a/include.txt" -temp="%cd%/temp"
+)
+
 rem ## Copy mod.cpp and key.
-copy /Y "%WORKING_DIR%\src\mod.cpp" "%WORKING_DIR%\build\mod.cpp"
-copy /Y "%WORKING_DIR%\src\keys\TaskForceVandalAuxMod.bikey" "%WORKING_DIR%\build\keys\TaskForceVandalAuxMod.bikey"
+copy /Y/V "%cd%\src\mod.cpp" "%cd%\build\mod.cpp"
+copy /Y/V "%cd%\src\keys\TaskForceVandalAuxMod.bikey" "%cd%\build\keys\TaskForceVandalAuxMod.bikey"
 rem ## Pause so user can see output.
 pause
