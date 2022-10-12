7a83d6be4a64d21c3f2f5e6dd2b489d5b3b95d4b
Peter <petergorman11@gmail.com>
2022-10-12 09:20:11 -0400

    Add Cessnas armor and helmet

--- src/addons/TFV_Armor/config.cpp
+++ src/addons/TFV_Armor/config.cpp
@@ -59,7 +59,11 @@ class CfgPatches
             "TFV_M52D_Vest_Basic_Probie",
 
             "TFV_ODST_Helmet_Foley",
-            "TFV_M52D_Vest_Basic_Foley"
+            "TFV_M52D_Vest_Basic_Foley",
+
+            "TFV_ODST_Helmet_Cessna",
+            "TFV_M52D_Vest_Basic_Cessna",
+            "TFV_M52D_Vest_Demolitions_Cessna"
         };
     };
 };
@@ -1642,6 +1646,63 @@ class CfgWeapons
         };
     };
     #pragma endregion Foley
+
+    #pragma region Cessna
+    class TFV_ODST_Helmet_Cessna: OPTRE_UNSC_CH252D_Helmet
+    {
+        author="Article 2 Studios & Gooman";
+        displayName="CH252D Helmet [Cessna]";
+        hiddenSelectionsTextures[]=
+        {
+            "TFV_Armor\data\cessna\cessna_odst_helmet_co.paa",
+			"TFV_Armor\data\cessna\cessna_odst_visor_co.paa"
+        };
+    };
+
+    class TFV_ODST_Helmet_Cessna_dp: OPTRE_UNSC_CH252D_Helmet_dp
+    {
+        scope=1;
+		scopeArsenal=1;
+		scopeCurator=1;
+        author="Article 2 Studios & Gooman";
+        displayName="CH252D Helmet [Cessna]";
+        hiddenSelectionsTextures[]=
+		{
+            "TFV_Armor\data\cessna\cessna_odst_helmet_co.paa"
+		};
+    };
+
+    #define VEST_CESSNA hiddenSelectionsTextures[]= \
+    { \
+        "TFV_Armor\data\common\m52d_underarmor_co.paa", \
+        "TFV_Armor\data\common\m52d_vest_co.paa", \
+        "TFV_Armor\data\cessna\cessna_odst_legs_co.paa", \
+        "optre_unsc_units\army\data\ghillie_woodland_co.paa", \
+        "TFV_Armor\data\cessna\cessna_odst_armor_co.paa", \
+    }
+
+    class TFV_M52D_Vest_Basic_Cessna: TFV_OPTRE_UNSC_M52D_Armor
+    {
+        displayName = "M52D Body Armor (Basic) (Cessna)";
+        VEST_CESSNA;
+        class ItemInfo: ItemInfo
+        {
+            VEST_CESSNA;
+        };
+    };
+
+    class TFV_M52D_Vest_Demolitions_Cessna: TFV_OPTRE_UNSC_M52D_Armor_Demolitions
+    {
+        displayName = "M52D Body Armor (Demolitions) (Cessna)";
+        VEST_CESSNA;
+        class ItemInfo: ItemInfo
+        {
+            VEST_CESSNA;
+        };
+    };
+
+    #undef VEST_CESSNA
+    #pragma endregion Cessna
 };
 
 class cfgMods
