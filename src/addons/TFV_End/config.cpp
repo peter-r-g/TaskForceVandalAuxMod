f77195e083376f2aa235045a0bf8f1a190d5feaf
peter-r-g <petergorman11@gmail.com>
2022-10-19 00:50:26 -0400

    Add TFV_End addon

---
+++ src/addons/TFV_End/config.cpp
@@ -0,0 +1,29 @@
+class CfgPatches
+{
+    class TFV_End
+    {
+        name = "Task Force Vandal Aux - End";
+        author = "Gooman";
+
+        requiredVersion = 0.1;
+        requiredAddons[] = {
+            "TFV_Main",
+            "TFV_Armor",
+            "TFV_Equipment",
+            "TFV_Items",
+            "TFV_Music",
+            "TFV_Vehicles",
+            "TFV_Weapons",
+            "TFV_Weapons_EX",
+            "TFV_Weapons_EX_Magazines"
+        };
+
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+class cfgMods
+{
+    author = "Gooman";
+};
