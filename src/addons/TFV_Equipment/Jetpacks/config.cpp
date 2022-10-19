11a9b8c97e7762540e5ead8ccabbe2f3b3c73f0c
peter-r-g <petergorman11@gmail.com>
2022-10-19 00:50:58 -0400

    Split equipment entries and add jetpacks

---
+++ src/addons/TFV_Equipment/Jetpacks/config.cpp
@@ -0,0 +1,167 @@
+class CfgPatches
+{
+    class TFV_Equipment_Jetpacks
+    {
+        name = "Task Force Vandal Aux - Equipment (Jetpacks)";
+        author = "Gooman";
+
+        requiredVersion = 0.1;
+        requiredAddons[] = {
+            "TFV_Main",
+            "NSM_patch_end"
+        };
+
+        units[] = {};
+        weapons[] = {};
+        vehicles[] = {
+            "TFV_Raider_Jetpack"
+        };
+    };
+};
+
+class CfgVehicles
+{
+    class NSM_neutral_XD_1_backpack;
+    class NSM_neutral_XD_1_LTU_backpack;
+
+    class TFV_Marauder_Jetpack : NSM_neutral_XD_1_LTU_backpack
+    {
+        author = "NSM & Gooman";
+        displayName = "Marauder Jetpack";
+        model = "\OPTRE_weapons\backpacks\jetpack.p3d";
+        maximumload = 600;
+        mass = 100;
+        hiddenSelections[] =
+        {
+            "camo1"
+        };
+        hiddenSelectionsTextures[] =
+        {
+            "NSM_Objects\Data\XD_1_JumpPack_CO.paa"
+        };
+        picture = "\NSM_Objects\icon.paa";
+
+        NSM_jumppack_is_jumppack = 1;
+        NSM_jumppack_spam_delay = 1;
+        NSM_jumppack_energy_capacity = 200;
+        NSM_jumppack_recharge = 0;
+        NSM_jumppack_jump_types[] = {
+            {
+                "Forward Jump", // Name of jump
+                {
+                    12,         // forward velo(0)
+                    20,         // verticle velo(1)
+                    50,         // cost(2)
+                    0,          // angle(3)
+                    0,          // directional(4)(no=0,yes=1)
+                    0           // can prone jump(5)(no=0,yes=1)
+                }
+            },
+            { "Short Jump", { 25, 7, 25, 0, 1, 1 } }
+        };
+
+        NSM_jumppack_jump_effect_script = "NSM_jumppack_effect_fnc_jt_21";
+        NSM_jumppack_effect_points[] = { { "spine3", { 0, -0.3, -0.1 } } };
+        NSM_jumppack_sound_ignite[] = { "NSM_Main\sounds\cdv21Start.ogg" };
+        NSM_jumppack_sound_land[] = { "NSM_Main\sounds\cdv21End.ogg" };
+        NSM_jumppack_sound_idle[] = { "NSM_Main\sounds\cdv21Idle.ogg" };
+    };
+
+    class TFV_Raider_Jetpack : NSM_neutral_XD_1_backpack
+    {
+        author = "NSM & Gooman";
+        displayName = "Raider Jetpack";
+        model = "\OPTRE_weapons\backpacks\jetpack.p3d";
+        maximumload = 600;
+        mass = 100;
+        hiddenSelections[] =
+        {
+            "camo1"
+        };
+        hiddenSelectionsTextures[] =
+        {
+            "NSM_Objects\Data\XD_1_JumpPack_CO.paa"
+        };
+        picture = "\NSM_Objects\icon.paa";
+
+        NSM_jumppack_is_jumppack = 1;
+        NSM_jumppack_spam_delay = 1;
+        NSM_jumppack_energy_capacity = 200;
+        NSM_jumppack_recharge = 5;
+        NSM_jumppack_jump_types[] = {
+            {
+                "Forward Jump", // Name of jump
+                {
+                    12,         // forward velo(0)
+                    20,         // verticle velo(1)
+                    50,         // cost(2)
+                    0,          // angle(3)
+                    0,          // directional(4)(no=0,yes=1)?
+                    0           // can prone jump(5)(no=0,yes=1)?
+                }
+            },
+            { "Short Jump", { 25, 7, 25, 0, 1, 1 } }
+        };
+
+        NSM_jumppack_jump_effect_script = "NSM_jumppack_effect_fnc_jt_21";
+        NSM_jumppack_effect_points[] = { { "spine3", { 0, -0.3, -0.1 } } };
+        NSM_jumppack_sound_ignite[] = { "NSM_Main\sounds\cdv21Start.ogg" };
+        NSM_jumppack_sound_land[] = { "NSM_Main\sounds\cdv21End.ogg" };
+        NSM_jumppack_sound_idle[] = { "NSM_Main\sounds\cdv21Idle.ogg" };
+    };
+
+    class TFV_Vandal_Jetpack : NSM_neutral_XD_1_backpack
+    {
+        author = "NSM & Gooman";
+        displayName = "Vandal Jetpack";
+        model = "\OPTRE_weapons\backpacks\jetpack.p3d";
+        maximumload = 600;
+        mass = 100;
+        hiddenSelections[] =
+        {
+            "camo1"
+        };
+        hiddenSelectionsTextures[] =
+        {
+            "NSM_Objects\Data\XD_1_JumpPack_CO.paa"
+        };
+        picture = "\NSM_Objects\icon.paa";
+
+        NSM_jumppack_is_jumppack = 1;
+        NSM_jumppack_spam_delay = 1;
+        NSM_jumppack_energy_capacity = 200;
+        NSM_jumppack_recharge = 5;
+        NSM_jumppack_jump_types[] = {
+            {
+                "Forward Jump", // Name of jump
+                {
+                    12,         // forward velo(0)
+                    20,         // verticle velo(1)
+                    50,         // cost(2)
+                    0,          // angle(3)
+                    0,          // directional(4)(no=0,yes=1)?
+                    0           // can prone jump(5)(no=0,yes=1)?
+                }
+            },
+            { "Short Jump", { 25, 7, 25, 0, 1, 1 } }
+        };
+
+        NSM_jumppack_jump_effect_script = "NSM_jumppack_effect_fnc_jt_21";
+        NSM_jumppack_effect_points[] = { { "spine3", { 0, -0.3, -0.1 } } };
+        NSM_jumppack_sound_ignite[] = { "NSM_Main\sounds\cdv21Start.ogg" };
+        NSM_jumppack_sound_land[] = { "NSM_Main\sounds\cdv21End.ogg" };
+        NSM_jumppack_sound_idle[] = { "NSM_Main\sounds\cdv21Idle.ogg" };
+
+        tf_encryptionCode = "tf_west_radio_code";
+        tf_dialog = "rt1523g_radio_dialog";
+        tf_subtype = "digital_lr";
+        tf_range = 25000;
+        tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+        tf_hasLRradio = 1;
+    };
+};
+
+class cfgMods
+{
+    author = "Gooman";
+};
--- src/addons/TFV_Equipment/config.cpp
+++ src/addons/TFV_Equipment/config.cpp
@@ -7,75 +7,12 @@ class CfgPatches
 
         requiredVersion = 0.1;
         requiredAddons[] = {
-            "TFV_Main",
-            "OPTRE_Weapons_Backpacks",
-            "NSM_patch_end"
+            "TFV_Main"
         };
 
-        units[] = {
-            "TFV_OPTRE_ILCS_Rucksack_LR",
-            "TFV_OPTRE_ONI_Rucksack"
-        };
+        units[] = {};
         weapons[] = {};
     };
-};
-
-class CfgVehicles
-{
-    #pragma region Base
-    class OPTRE_ILCS_Rucksack_Black;
-    class OPTRE_UNSC_Rucksack;
-    class RTO_11th_ILCS_ANPRC_515;
-    #pragma endregion Base
-
-    #pragma region Backpacks
-    class TFV_OPTRE_ILCS_Rucksack_LR: OPTRE_ILCS_Rucksack_Black
-    {
-        author = "Article 2 Studios & Gooman";
-        displayName = "ILCS Rucksack [LR]";
-        descriptionShort = "ILCS Rucksack [LR]<br>ODST Long Range Radio<br>25km Effective Range";
-        maximumLoad=600;
-		mass=50;
-        tf_encryptionCode = "tf_west_radio_code";
-		tf_dialog = "rt1523g_radio_dialog";
-		tf_subtype = "digital_lr";
-		tf_range = 25000;
-		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
-		tf_hasLRradio = 1;
-    };
-
-    class TFV_OPTRE_ONI_Rucksack: OPTRE_UNSC_Rucksack
-    {
-        author = "Article 2 Studios & Gooman";
-        displayName = "ONI Rucksack";
-        descriptionShort = "ONI Rucksack<br>ONI Long Range Radio<br>25km Effective Range";
-        maximumLoad=800;
-		mass=50;
-        tf_encryptionCode = "tf_west_radio_code";
-		tf_dialog = "rt1523g_radio_dialog";
-		tf_subtype = "digital_lr";
-		tf_range = 25000;
-		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
-		tf_hasLRradio = 1;
-    };
-
-    class TFV_11th_ILCS_ANPRC_515: RTO_11th_ILCS_ANPRC_515
-    {
-        author = "1st MEU & Gooman";
-        displayName = "ONI ILCS ANPRC-515";
-        maximumLoad=800;
-        mass=50;
-        tf_encryptionCode = "tf_west_radio_code";
-		tf_dialog = "rt1523g_radio_dialog";
-		tf_subtype = "digital_lr";
-		tf_range = 25000;
-		tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
-		tf_hasLRradio = 1;
-    };
-    #pragma endregion Backpacks
-
-    #pragma region Jetpacks
-    #pragma endregion Jetpacks
 };
 
 class cfgMods
