afbe0d8852f5cf7cfda2604d1737057037c947c1
Jonathan Chu <bobchu351@gmail.com>
2022-11-16 12:52:25 +0800

    Add Pelican + resupply (#2)

--- src/addons/TFV_Vehicles/config.cpp
+++ src/addons/TFV_Vehicles/config.cpp
@@ -2,203 +2,331 @@ class CfgPatches
 {
 	class TFV_Vehicles
 	{
-		author="1st MEU Mod Team";
-		requiredAddons[]=
-		{
-			"1st_MEU_patch_main"
-		};
-		requiredVersion=0.1;
 		units[]=
 		{
-			"TFV_Speedbag"
+			"TFV_Ammo_Rack_Weapons",
 		};
 		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[]=
+		{
+			"OPTRE_Core",
+			"TFV_Weapons",
+			"TFV_Weapons_EX",
+			"TFV_Weapons_EX_Magazines",
+			"OPTRE_UNSC_Units",
+			"OPTRE_Ins_Units"
+		};
 	};
 };
-
 class CfgVehicles
 {
-	class Land_optre_milcrate_h3_long;
-	class TFV_Speedbag: Land_optre_milcrate_h3_long
+	class NATO_Box_Base;
+	class TFV_Ammo_Box_Base: NATO_Box_Base
 	{
-		scope=2;
-		scopeCurator=2;
-		scopeEditor=2;
-		displayName="[TFV] Speedbag";
-		author="1stMEU Mark + Serin";
-		editorCategory="MEU_Objects";
-		editorSubcategory="Objects";
-		ace_cargo_canLoad=1;
-		ace_cargo_size=1;
-		ace_dragging_canDrag=1;
-		ace_dragging_dragPosition[]={0,1.2,0};
-		ace_dragging_dragDirection=0;
-		ace_dragging_canCarry=1;
-		ace_dragging_carryPosition[]={0,1.2,0};
-		ace_dragging_carryDirection=0;
-		mass=40;
-		maximumLoad=696969;
+		dlc="OPTRE";
+		scope=1;
+		vehicleClass="OPTRE_UNSC_Ammo_class";
+		displayName="-";
+		model="\A3\weapons_F\AmmoBoxes\Supplydrop";
+		author="Article 2 Studios";
+		icon="iconCrateWpns";
+		transportMaxWeapons=1000;
+		transportMaxMagazines=5000;
+		maximumLoad=50000;
+		transportMaxBackpacks=100;
+		transportAmmo=1;
+		transportRepair=1;
+		transportFuel=1;
+		supplyRadius=2.5;
+		armor=1000;
 		class TransportMagazines
 		{
-			class _xx_OPTRE_60Rnd_762x51_Mag_APT
+		};
+		class TransportWeapons
+		{
+		};
+		class TransportItems
+		{
+		};
+	};
+	class TFV_Ammo_Rack_Weapons: TFV_Ammo_Box_Base
+	{
+		dlc="OPTRE";
+		scope=2;
+		displayName="[TFV] Weapons Rack";
+		hiddenSelections[]=
+		{
+			"attach_Cover"
+		};
+		model="\OPTRE_Buildings\Containers\optre_milcrate_reachgreencrate";
+		author="Article 2 Studios";
+		transportMaxWeapons=50;
+		transportMaxMagazines=1000;
+		maximumLoad=500000;
+		transportMaxBackpacks=10;
+		transportAmmo=1;
+		transportRepair=0;
+		transportFuel=0;
+		supplyRadius=2.5;
+		armor=500;
+		class TransportMagazines
+		{
+			class xx_TFV_60Rnd_762x51_Mag
 			{
-				magazine="OPTRE_60Rnd_762x51_Mag_APT";
-				count=20;
+				magazine="TFV_60Rnd_762x51_Mag";
+				count=100;
 			};
-			class _xx_MEU_762SLAP_32_Mag_T
+			class xx_TFV_60Rnd_762x51_Mag_Tracer
 			{
-				magazine="MEU_762SLAP_32_Mag_T";
-				count=20;
+				magazine="TFV_60Rnd_762x51_Mag_Tracer";
+				count=25;
 			};
-			class _xx_30rnd_556x45_mag_APT
+			class xx_TFV_32Rnd_762x51_Mag
 			{
-				magazine="30rnd_556x45_mag_APT";
-				count=20;
+				magazine="TFV_32Rnd_762x51_Mag";
+				count=50;
 			};
-			class _xx_30rnd_65x39_mag_APT
+			class xx_TFV_32Rnd_762x51_Mag_Tracer
 			{
-				magazine="30rnd_65x39_mag_APT";
-				count=20;
+				magazine="TFV_32Rnd_762x51_Mag_Tracer";
+				count=25;
 			};
-			class _xx_30rnd_65x39_mag_msbs_APT
+			class xx_TFV_36Rnd_95x40_Mag
 			{
-				magazine="30rnd_65x39_mag_msbs_APT";
-				count=20;
+				magazine="TFV_36Rnd_95x40_Mag";
+				count=30;
 			};
-			class _xx_1Rnd_HEDP_MEU_shell
+			class xx_TFV_36Rnd_95x40_Mag_Tracer
 			{
-				magazine="1Rnd_HEDP_MEU_shell";
+				magazine="TFV_36Rnd_95x40_Mag_Tracer";
+				count=15;
+			};
+			class xx_TFV_15Rnd_762x51_Mag
+			{
+				magazine="TFV_15Rnd_762x51_Mag";
+				count=40;
+			};
+			class xx_TFV_100Rnd_95x40_Box
+			{
+				magazine="TFV_100Rnd_95x40_Box";
 				count=10;
 			};
-			class _xx_3Rnd_HE_MEU_shell
+			class xx_TFV_100Rnd_95x40_Box_Tracer
 			{
-				magazine="3Rnd_HE_MEU_shell";
+				magazine="TFV_100Rnd_95x40_Box_Tracer";
+				count=5;
+			};
+			class xx_TFV_200Rnd_95x40_Box
+			{
+				magazine="TFV_200Rnd_95x40_Box";
 				count=10;
+			};
+			class xx_TFV_200Rnd_95x40_Box_Tracer
+			{
+				magazine="TFV_200Rnd_95x40_Box_Tracer";
+				count=5;
+			};
+			class xx_TFV_8Rnd_127x40_Mag
+			{
+				magazine="TFV_8Rnd_127x40_Mag";
+				count=25;
+			};
+			class xx_TFV_8Rnd_127x40_AP_Mag
+			{
+				magazine="TFV_8Rnd_127x40_AP_Mag";
+				count=5;
+			};
+			class xx_TFV_8Rnd_127x40_Mag_Tracer
+			{
+				magazine="TFV_8Rnd_127x40_Mag_Tracer";
+				count=10;
+			};
+			class xx_TFV_6Rnd_8Gauge_Pellets
+			{
+				magazine="TFV_6Rnd_8Gauge_Pellets";
+				count=25;
+			};
+			class xx_TFV_6Rnd_8Gauge_Slugs
+			{
+				magazine="TFV_6Rnd_8Gauge_Slugs";
+				count=25;
+			};
+			class xx_TFV_60Rnd_5x23mm_Mag
+			{
+				magazine="TFV_60Rnd_5x23mm_Mag";
+				count=30;
+			};
+			class xx_TFV_60Rnd_5x23mm_Mag_tracer
+			{
+				magazine="TFV_60Rnd_5x23mm_Mag_tracer";
+				count=15;
+			};
+			class xx_TFV_4Rnd_145x114_APFSDS_Mag
+			{
+				magazine="TFV_4Rnd_145x114_APFSDS_Mag";
+				count=8;
+			};
+			class xx_TFV_M41_Twin_HEAT
+			{
+				magazine="TFV_M41_Twin_HEAT";
+				count=6;
+			};
+			class xx_TFV_M41_Twin_HEAT_G
+			{
+				magazine="TFV_M41_Twin_HEAT_G";
+				count=2;
+			};
+			class xx_TFV_M41_Twin_HEAP
+			{
+				magazine="TFV_M41_Twin_HEAP";
+				count=6;
+			};
+			// speedbag contents
+			class _xx_TFV_60Rnd_762x51_Mag_APT
+			{
+				magazine="TFV_60Rnd_762x51_Mag_APT";
+				count=20;
 			};
 			class _xx_3Rnd_Smoke_Grenade_shell
 			{
 				magazine="3Rnd_Smoke_Grenade_shell";
 				count=10;
 			};
-			class _xx_OPTRE_36Rnd_95x40_Mag_HPSAPT
+			class _xx_TFV_36Rnd_95x40_Mag_HPSAPT
 			{
-				magazine="OPTRE_36Rnd_95x40_Mag_HPSAPT";
+				magazine="TFV_36Rnd_95x40_Mag_HPSAPT";
 				count=20;
 			};
-			class _xx_OPTRE_20Rnd_86x70_Mag_JHPT
+			class _xx_TFV_20Rnd_86x70_Mag_JHPT
 			{
-				magazine="OPTRE_20Rnd_86x70_Mag_JHPT";
+				magazine="TFV_20Rnd_86x70_Mag_JHPT";
 				count=20;
 			};
-			class _xx_OPTRE_20Rnd_86x70_Mag_APT
+			class _xx_TFV_20Rnd_86x70_Mag_APT
 			{
-				magazine="OPTRE_20Rnd_86x70_Mag_APT";
+				magazine="TFV_20Rnd_86x70_Mag_APT";
 				count=20;
 			};
-			class _xx_OPTRE_12Rnd_8Gauge_Pellets
+			class _xx_TFV_12Rnd_8Gauge_Pellets
 			{
-				magazine="OPTRE_12Rnd_8Gauge_Pellets";
+				magazine="TFV_12Rnd_8Gauge_Pellets";
 				count=20;
 			};
-			class _xx_OPTRE_12Rnd_8Gauge_Slugs
+			class _xx_TFV_12Rnd_8Gauge_Slugs
 			{
-				magazine="OPTRE_12Rnd_8Gauge_Slugs";
+				magazine="TFV_12Rnd_8Gauge_Slugs";
 				count=20;
 			};
-			class _xx_OPTRE_60Rnd_5x23mm_Mag_FMJT
+			class _xx_TFV_60Rnd_5x23mm_Mag_FMJT
 			{
-				magazine="OPTRE_60Rnd_5x23mm_Mag_FMJT";
+				magazine="TFV_60Rnd_5x23mm_Mag_FMJT";
 				count=20;
 			};
-			class _xx_OPTRE_15Rnd_DMR_762x51_Mag_APT
+			class _xx_TFV_15Rnd_762x51_Mag_APT
 			{
-				magazine="OPTRE_15Rnd_DMR_762x51_Mag_APT";
+				magazine="TFV_15Rnd_762x51_Mag_APT";
 				count=20;
 			};
-			class _xx_OPTRE_15Rnd_DMR_762x51_Mag_JHPT
+			class _xx_TFV_15Rnd_762x51_Mag_JHPT
 			{
-				magazine="OPTRE_15Rnd_DMR_762x51_Mag_JHPT";
+				magazine="TFV_15Rnd_762x51_Mag_JHPT";
 				count=20;
 			};
-			class _xx_OPTRE_4Rnd_145x114_Mag_APFSDST
+			class _xx_TFV_4Rnd_145x114_Mag_APFSDST
 			{
-				magazine="OPTRE_4Rnd_145x114_Mag_APFSDST";
+				magazine="TFV_4Rnd_145x114_Mag_APFSDST";
 				count=20;
 			};
-			class _xx_OPTRE_4Rnd_145x114_Mag_HEDPT
+			class _xx_TFV_4Rnd_145x114_Mag_HEDPT
 			{
-				magazine="OPTRE_4Rnd_145x114_Mag_HEDPT";
+				magazine="TFV_4Rnd_145x114_Mag_HEDPT";
 				count=10;
 			};
-			class _xx_OPTRE_4Rnd_145x114_Mag_NARQ
+			class _xx_TFV_4Rnd_145x114_Mag_NARQ
 			{
-				magazine="OPTRE_4Rnd_145x114_Mag_NARQ";
+				magazine="TFV_4Rnd_145x114_Mag_NARQ";
 				count=2;
 			};
-			class _xx_OPTRE_4Rnd_145x114_Mag_HVAPT
+			class _xx_TFV_4Rnd_145x114_Mag_HVAPT
 			{
-				magazine="OPTRE_4Rnd_145x114_Mag_HVAPT";
+				magazine="TFV_4Rnd_145x114_Mag_HVAPT";
 				count=10;
 			};
-			class _xx_OPTRE_100Rnd_127x99_TRACER_M250HMG
+			class _xx_TFV_200Rnd_95x40_Box_HPSAPT
 			{
-				magazine="OPTRE_100Rnd_127x99_TRACER_M250HMG";
+				magazine="TFV_200Rnd_95x40_Box_HPSAPT";
 				count=10;
 			};
-			class _xx_OPTRE_100Rnd_127x99_TRACER_HE_M250HMG
+			class _xx_TFV_200Rnd_95x40_Box_JHPT
 			{
-				magazine="OPTRE_100Rnd_127x99_TRACER_HE_M250HMG";
+				magazine="TFV_200Rnd_95x40_Box_JHPT";
 				count=10;
 			};
-			class _xx_OPTRE_200Rnd_95x40_Box_HPSAPT
+			class _xx_TFV_1Rnd_50x137_PEN
 			{
-				magazine="OPTRE_200Rnd_95x40_Box_HPSAPT";
-				count=10;
-			};
-			class _xx_OPTRE_200Rnd_95x40_Box_JHPT
-			{
-				magazine="OPTRE_200Rnd_95x40_Box_JHPT";
-				count=10;
-			};
-			class _xx_MEU_200Rnd_556_Mag_Tracer
-			{
-				magazine="MEU_200Rnd_556_Mag_Tracer";
-				count=10;
-			};
-			class _xx_OPTRE_1Rnd_50x137_PEN
-			{
-				magazine="OPTRE_1Rnd_50x137_PEN";
+				magazine="TFV_1Rnd_50x137_PEN";
 				count=1;
 			};
-			class _xx_OPTRE_M41_Twin_HEAT_Thermal
+			class _xx_TFV_M41_Twin_HEAT_Thermal
 			{
-				magazine="OPTRE_M41_Twin_HEAT_Thermal";
+				magazine="TFV_M41_Twin_HEAT_Thermal";
 				count=2;
 			};
-			class _xx_OPTRE_M41_Twin_HEAT_SACLOS
+			class _xx_TFV_M41_Twin_HEAT_SACLOS
 			{
-				magazine="OPTRE_M41_Twin_HEAT_SACLOS";
+				magazine="TFV_M41_Twin_HEAT_SACLOS";
 				count=2;
 			};
-			class _xx_OPTRE_M41_Twin_HEAT_SALH
+			class _xx_TFV_M41_Twin_HEAT_SALH
 			{
-				magazine="OPTRE_M41_Twin_HEAT_SALH";
+				magazine="TFV_M41_Twin_HEAT_SALH";
 				count=2;
 			};
-			class _xx_OPTRE_M41_Twin_HEAT
+			class _xx_TFV_M41_Twin_HEAT
 			{
-				magazine="OPTRE_M41_Twin_HEAT";
+				magazine="TFV_M41_Twin_HEAT";
 				count=2;
 			};
-			class _xx_ACE_SpareBarrel
+			class xx_OPTRE_M9_Frag
 			{
-				magazine="ACE_SpareBarrel";
+				magazine="OPTRE_M9_Frag";
+				count=25;
+			};
+			class xx_OPTRE_M2_Smoke
+			{
+				magazine="OPTRE_M2_Smoke";
+				count=25;
+			};
+			class xx_OPTRE_M2_Smoke_Blue
+			{
+				magazine="OPTRE_M2_Smoke_Blue";
 				count=10;
 			};
-			class _xx_test2_m250_100rnd_ap
+			class xx_OPTRE_M2_Smoke_Yellow
 			{
-				magazine="test2_m250_100rnd_ap";
-				count=5;
+				magazine="OPTRE_M2_Smoke_Yellow";
+				count=10;
+			};
+			class xx_OPTRE_M2_Smoke_Red
+			{
+				magazine="OPTRE_M2_Smoke_Red";
+				count=10;
+			};
+			class xx_OPTRE_M2_Smoke_Green
+			{
+				magazine="OPTRE_M2_Smoke_Green";
+				count=10;
+			};
+			class xx_OPTRE_M2_Smoke_Orange
+			{
+				magazine="OPTRE_M2_Smoke_Orange";
+				count=10;
+			};
+			class xx_OPTRE_M2_Smoke_Purple
+			{
+				magazine="OPTRE_M2_Smoke_Purple";
+				count=10;
 			};
 		};
 		class TransportItems
